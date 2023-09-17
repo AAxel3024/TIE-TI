680c8aa4fc3a250370add1d672ce0868deb6d6c6
AXEL ARTEAGA CEDEÑO <e1316854965@live.uleam.edu.ec>
2023-09-17 14:07:38 -0500

    conf

--- tienda-online/Config/Config.php
+++ tienda-online/Config/Config.php
@@ -1,16 +1,16 @@
 <?php
-const BASE_URL = "http://localhost/tienda-online/";
-const HOST = "localhost";
-const USER = "root";
-const PASS = "";
+//const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "tienda-online2023.mysql.database.azure.com";
+const USER = "axel30";
+const PASS = "#Bonjie-3024";
 const DB = "tienda-online";
 const CHARSET = "charset=utf8";
-const TITLE = "TIENDA ONLINE";
-const MONEDA = "USD";
-const CLIENT_ID = "AXoIEDX2Xn49aZ946mLR1kIbzWu9lnZN6RiT3K3SxVVN7YF-HCkJae_-jWVcPBZNbMYYcoYQKf9I1OLW";
+//const TITLE = "TIENDA ONLINE";
+//const MONEDA = "USD";
+//const CLIENT_ID = "AXoIEDX2Xn49aZ946mLR1kIbzWu9lnZN6RiT3K3SxVVN7YF-HCkJae_-jWVcPBZNbMYYcoYQKf9I1OLW";
 
-const USER_SMTP = "axeladonisarteagaedeno@gmail.com";
-const PASS_SMTP = "pdzwygypzempqgsr";
-const PUERTO_SMTP = 465;
-const HOST_SMTP = "smtp.gmail.com";
+//const USER_SMTP = "axeladonisarteagaedeno@gmail.com";
+//const PASS_SMTP = "pdzwygypzempqgsr";
+//const PUERTO_SMTP = 465;
+//const HOST_SMTP = "smtp.gmail.com";
 ?>
