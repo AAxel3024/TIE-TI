43f694dda4987c1574753e8d3274996f16e632a7
AXEL ARTEAGA CEDEÑO <e1316854965@live.uleam.edu.ec>
2023-09-17 13:07:39 -0500

    tienda-online

---
+++ tienda-online/Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AXoIEDX2Xn49aZ946mLR1kIbzWu9lnZN6RiT3K3SxVVN7YF-HCkJae_-jWVcPBZNbMYYcoYQKf9I1OLW";
+
+const USER_SMTP = "axeladonisarteagaedeno@gmail.com";
+const PASS_SMTP = "pdzwygypzempqgsr";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
